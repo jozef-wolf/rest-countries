a1c0e6fb4f80f5dc4096d5e2148e753c427025e5
jozef-wolf <wolf.joozef@gmail.com>
2021-07-03 15:01:53 +0200

    getCountries / async await

---
+++ main.js
@@ -0,0 +1,7 @@
+const getCountries = async () => {
+  const response = await fetch("https://restcountries.eu/rest/v2/all");
+  const data = await response.json();
+
+  return data;
+};
+getCountries().then((data) => console.log("resolved", data));
