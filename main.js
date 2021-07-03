f506f24c325b6905ea71b733542adc82ec8c89ae
jozef-wolf <wolf.joozef@gmail.com>
2021-07-03 15:09:32 +0200

    catch error  / URL

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const getCountries = async () => {
   const response = await fetch("https://restcountries.eu/rest/v2/all");
   const data = await response.json();
 
+  if (response.status !== 200) {
+    throw new Error("cannot fetch the data" );
+  }
+
   return data;
 };
 
 getCountries()
   .then((data) => console.log("resolved", data))
-  .catch((err) => console.log("resolved:", err.message));
+  .catch((err) => console.log("rejected:", err.message));
