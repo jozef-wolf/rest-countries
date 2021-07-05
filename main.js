b1d222f5fd1e0790ea969344d7a861f98097c089
jozef-wolf <wolf.joozef@gmail.com>
2021-07-05 17:20:25 +0200

    template / Countries rendered

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
+const countriesContainer = document.querySelector(".container__table");
+
 const getCountries = async () => {
-  const response = await fetch("https://restcountries.eu/rest/v2/all");
-  const data = await response.json();
+  let urlCountries = `
+https://restcountries.eu/rest/v2/all
+
+`;
+  let templateCountries = "";
+
+  const response = await fetch(urlCountries);
+  const countries = await response.json();
+
+  countries.forEach((country) => {
+    templateCountries += `<div>${country.name}</div>
+    <div>${country.currencies[0].name}</div>
+    <div>${country.languages}</div>
+    <div>${country.population}</div>
+    <div>${country.area}</div>
+    <div><img src='${country.flag}'></img></div>`;
+  });
+  countriesContainer.innerHTML = templateCountries;
 
   if (response.status !== 200) {
     throw new Error("cannot fetch the data");
   }
 
-  return data;
+  return countries;
 };
 
 getCountries()
   .then((data) => console.log("resolved", data))
   .catch((err) => console.log("rejected:", err.message));
 
-const table = document.querySelector("#table");
+window.addEventListener("DOMContentLoaded", () => getCountries());
 
 const mappedContentForHeader = {
   name: "Name",
