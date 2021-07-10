b706939f1349a3ccaef6e78e6963ecd939ce08c7
jozef-wolf <wolf.joozef@gmail.com>
2021-07-10 18:08:00 +0200

    wip - 2 event listeners / sort

--- sort.js
+++ sort.js
@@ -26,3 +26,30 @@ document.querySelectorAll("th").forEach((th) =>
 );
 
 //mobile
+
+const getCellValueM = (div, idx) =>
+  div.children[idx].innerText || div.children[idx].textContent;
+
+const comparerM = (idx, asc) => (a, b) =>
+  ((v1, v2) =>
+    v1 !== "" && v2 !== "" && !isNaN(v1) && !isNaN(v2)
+      ? v1 - v2
+      : v1.toString().localeCompare(v2))(
+    getCellValueM(asc ? a : b, idx),
+    getCellValueM(asc ? b : a, idx)
+  );
+
+// do the work...
+document.querySelectorAll("th").forEach((th) =>
+  th.addEventListener("mouseover", () => {
+    const table = th.closest(".container");
+    Array.from(table.querySelectorAll("div:nth-child(n+2)"))
+      .sort(
+        comparerM(
+          Array.from(th.parentNode.children).indexOf(th),
+          (this.asc = !this.asc)
+        )
+      )
+      .forEach((div) => table.appendChild(div));
+  })
+);
